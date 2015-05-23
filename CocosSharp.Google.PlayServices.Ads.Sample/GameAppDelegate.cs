486677fcb649e5db98452ed2294388ea57d9e4b3
Peter Strong <xjpeter@gmail.com>
2015-05-23 14:46:45 +0100

    Initial commit.

---
+++ CocosSharp.Google.PlayServices.Ads.Sample/GameAppDelegate.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using CocosSharp;
+using Android.Content;
+
+
+namespace CocosSharp.Google.PlayServices.Ads.Sample
+{
+    public class GameAppDelegate : CCApplicationDelegate
+    {
+        private readonly Context _context;
+
+        public GameAppDelegate(Context context)
+        {
+            _context = context;
+        }
+
+        public override async void ApplicationDidFinishLaunching(CCApplication application, CCWindow mainWindow)
+        {
+            application.PreferMultiSampling = false;
+            application.ContentRootDirectory = "Content";
+            application.ContentSearchPaths.Add("animations");
+            application.ContentSearchPaths.Add("fonts");
+            application.ContentSearchPaths.Add("sounds");
+
+            CCSize windowSize = mainWindow.WindowSizeInPixels;
+
+            float desiredWidth = 1024.0f;
+            float desiredHeight = 768.0f;
+            
+            // This will set the world bounds to be (0,0, w, h)
+            // CCSceneResolutionPolicy.ShowAll will ensure that the aspect ratio is preserved
+            CCScene.SetDefaultDesignResolution(desiredWidth, desiredHeight, CCSceneResolutionPolicy.ShowAll);
+            
+            // Determine whether to use the high or low def versions of our images
+            // Make sure the default texel to content size ratio is set correctly
+            // Of course you're free to have a finer set of image resolutions e.g (ld, hd, super-hd)
+            if (desiredWidth < windowSize.Width)
+            {
+                application.ContentSearchPaths.Add("images/hd");
+                CCSprite.DefaultTexelToContentSizeRatio = 2.0f;
+            }
+            else
+            {
+                application.ContentSearchPaths.Add("images/ld");
+                CCSprite.DefaultTexelToContentSizeRatio = 1.0f;
+            }
+            
+            CCScene scene = new CCScene(mainWindow);
+
+            var advertisingService = new GoogleAdvertisingService(_context);
+
+            advertisingService.Closed += (object sender, EventArgs e) =>
+            {
+                GameLayer gameLayer = new GameLayer();
+
+                scene.AddChild(gameLayer);
+            };
+
+            advertisingService.ShowInterstitial();
+
+            mainWindow.RunWithScene(scene);
+        }
+
+        public override void ApplicationDidEnterBackground(CCApplication application)
+        {
+            application.Paused = true;
+        }
+
+        public override void ApplicationWillEnterForeground(CCApplication application)
+        {
+            application.Paused = false;
+        }
+    }
+}
---
+++ CocosSharp.Google.PlayServices.Ads.Sample/GoogleAdvertisingService.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+using CocosSharp;
+
+using System.Threading.Tasks;
+
+using System.Threading;
+
+using Android.Gms.Ads;
+using Android.App;
+using Android.Content;
+using System.Diagnostics;
+
+namespace CocosSharp.Google.PlayServices.Ads.Sample
+{
+    public class GoogleAdvertisingService
+    {
+        private const string admobId = "<Get your ID at google.com/ads/admob>";
+
+        private readonly Context _context;
+
+        public GoogleAdvertisingService(Context context)
+        {
+            _context = context;
+        }
+
+        public event EventHandler Closed;
+
+        public void ShowInterstitial()
+        {
+            var adInterstitial = new InterstitialAd(_context)
+                {
+                    AdUnitId = admobId
+                };
+
+            var request = new AdRequest.Builder()
+                .AddTestDevice(AdRequest.DeviceIdEmulator)       // Simulator.
+                .Build();
+
+            var intlistener = new adlistener();
+
+            intlistener.AdLoaded += () =>
+            {
+                Application.SynchronizationContext.Post(_ =>
+                    {
+                        if (adInterstitial.IsLoaded)
+                        {
+                            adInterstitial.Show();
+                        }
+                    }, 
+                    null);
+            };
+
+            intlistener.AdClosed += () => 
+                {
+                    Debug.WriteLine("GoogleAdvertisingService - Closed");
+
+                    var closed = Closed;
+                    if(closed != null)
+                    {
+                        closed(this, EventArgs.Empty);
+                    }
+                };
+
+            intlistener.AdFailedToLoad += () =>
+                {
+                    Debug.WriteLine("GoogleAdvertisingService - FailedToLoad");
+                };
+
+            adInterstitial.AdListener = intlistener;
+
+            adInterstitial.LoadAd(request);
+        }
+
+        class adlistener : AdListener
+        {
+            // Declare the delegate (if using non-generic pattern). 
+            public delegate void AdLoadedEvent();
+            public delegate void AdClosedEvent();
+            public delegate void AdOpenedEvent();
+
+            // Declare the event. 
+            public event AdLoadedEvent AdLoaded;
+            public event AdClosedEvent AdClosed;
+            public event AdOpenedEvent AdOpened;
+            public event AdOpenedEvent AdFailedToLoad;
+
+            public override void OnAdLoaded()
+            {
+                if (AdLoaded != null)
+                {
+                    this.AdLoaded();
+                }
+
+                base.OnAdLoaded();
+            }
+
+            public override void OnAdClosed()
+            {
+                if (AdClosed != null)
+                {
+                    this.AdClosed();
+                }
+
+                base.OnAdClosed();
+            }
+
+            public override void OnAdOpened()
+            {
+                if (AdOpened != null)
+                {
+                    this.AdOpened();
+                }
+
+                base.OnAdOpened();
+            }
+
+            public override void OnAdFailedToLoad(int errorCode)
+            {
+                if (AdFailedToLoad != null)
+                {
+                    this.AdFailedToLoad();
+                }
+
+                base.OnAdFailedToLoad(errorCode);
+            }
+        }
+    }
+}
